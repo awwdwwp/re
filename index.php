0cd338b23385183dedac340c2360d34d03eca4af
awwdwwp <Bobonichandrin@gmail.com>
2025-03-31 00:47:22 +0200

    Assassin's Creed
    Styling the website to match the theme

--- index.php
+++ index.php
@@ -10,14 +10,19 @@
 	<link href="img/favicon.ico" rel="shortcut icon"/>
 
 	<!-- Google Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i" rel="stylesheet">
 
 	<!-- Stylesheets -->
-	<link rel="stylesheet" href="css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="css/font-awesome.min.css"/>
-	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<!-- <link rel="stylesheet" href="css/bootstrap.min.css"/> -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!-- <link rel="stylesheet" href="css/font-awesome.min.css"/> -->
+	<!-- <link rel="stylesheet" href="css/owl.carousel.css"/> -->
 	<link rel="stylesheet" href="css/style.css"/>
-	<link rel="stylesheet" href="css/animate.css"/>
+	<!-- <link rel="stylesheet" href="css/animate.css"/> -->
+	<link rel="stylesheet" href="css/carousel.css"/>
 
 
 	<!--[if lt IE 9]>
@@ -35,10 +40,65 @@
 	<!-- Header section -->
 	<?php include 'parts/header.html'; ?>
 	<!-- Header section end -->
+	<!-- <div data-ccid="6DlojvIEfV0lkEzMXCfKTQ" class="moduleCarousel module--media-hero-video-carousel module--seen module--enter-done"><div class="carousel carousel--looping-thumbnail" data-ccid-m="6m3Oz4FBlqIDtNQBbGVm1I" data-ccm-m="carouselMaster"><div class="carousel__slides" data-ccid-l="7bWzSujr9USnRRuLOZHDLk" data-ccm-l="carousel"><div class="carouselSlide carousel--looping-thumbnail carouselSlide--3" data-ccid-l="37R33mQ1pZzUerJoH2OTSo" data-ccm-l="carouselSlide"><div class="carouselSlide__slides__slide" style="background-image: url(&quot;https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/1aecXJZGAIIdettbCPkACO/4636e21dca7a01e41181ca22126989c1/Desktop_ACSH_Takeover_Slider3.jpg&quot;);"><div class="carouselSlide__slides__slide__media" style="background-color: rgb(158, 12, 18);"></div><div class="carouselSlide__slides__slide__content"><h2 data-ccf-l="title" style="color: rgb(158, 12, 18);">Discover the digital deluxe edition</h2><div data-ccf-l="content"><p>Enhance your game experience with additional digital content, including legendary gear and weapon sets for both Naoe and Yasuke.</p></div><a aria-label="Buy Now" class="sc-gggouf iWJvHo btn btn-primary carouselSlide__slides__slide__content__btn btn__number--1 external" data-innertext="Buy Now" data-ccid-m="1FZ6g33gyb3qbdqKV30VR0" data-ccm-m="buttonMaster" href="https://store.ubisoft.com/673b49019baa3f5f7f025a29.html" target="blank" rel="noreferrer noopener "><span class="" data-innertext="Buy Now" data-ccid-l="1B8ljSt9h50BSQZWaLJcqi" data-ccm-l="button" data-ccf-l="buttonText">Buy Now</span></a><button aria-label="Watch trailer" class="sc-cyQzhP jPNGvp btn btn-primary carouselSlide__slides__slide__content__btn btn__number--2 youtube-modal" data-innertext="Watch trailer" data-ccid-m="67menp7aIlg2oOWIrZOIP7" data-ccm-m="buttonMaster" type="button"><span class="" data-innertext="Watch trailer" data-ccid-l="2glkqsPmioPKDM7BRrd2z2" data-ccm-l="button" data-ccf-l="buttonText">Watch trailer</span></button><div class="btn btn-primary carouselSlide__slides__slide__content__btn btn__number--3 no-action has-image" data-ccid-m="5PVYg8EzBO89CdLvQiZxtk" data-ccm-m="buttonMaster"><img class="" src="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/2MJoL1KfnEjtJxKpWfEUyu/c53a985a90bb0f421c6d828e91a711b1/REDlogo_Vertical_AllWhite_NoCrest.png" alt="" loading="lazy"></div></div></div></div></div><div class="carousel__tabs"><div class="carousel__tabs__timer"><ul><li class=""><button class="carousel__tabs__btn" data-ccm-m="buttonMaster" type="button"><h2 data-innertext="Watch the Launch Trailer of AC Shadows">Watch the Launch Trailer of AC Shadows</h2><p>AC Shadows is available now on PS5, Xbox Series X|S, PC including Steam, and more. Buy now!</p><div class=""><img src="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/20UYtJu5B4St2jVasWOwSz/9492318e3ab35aa92ebde7afc0b645d5/ACSH_Crest_logo_300x378.png" alt="" loading="lazy"><span class="carousel__underline" style="background-color: rgb(158, 12, 18);"></span></div></button></li><li class=""><button class="carousel__tabs__btn" data-ccm-m="buttonMaster" type="button"><h2 data-innertext="Discover the upcoming expansion of AC Shadows">Discover the upcoming expansion of AC Shadows</h2><p>Travel to a new distinctive region - the island of Awaji - a place of mysterious beauty. Feel the tension of being hunted by a new faction, threatening the mysterious island of Awaji. Coming later in 2025.</p><div class=""><img src="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/20UYtJu5B4St2jVasWOwSz/9492318e3ab35aa92ebde7afc0b645d5/ACSH_Crest_logo_300x378.png" alt="" loading="lazy"><span class="carousel__underline" style="background-color: rgb(158, 12, 18);"></span></div></button></li><li class="is-active"><div class="progress-bar" style="width: 92%; background-color: rgb(158, 12, 18);"></div><progress value="9.2" max="10"></progress><button class="carousel__tabs__btn" data-ccm-m="buttonMaster" type="button"><h2 data-innertext="Discover the digital deluxe edition">Discover the digital deluxe edition</h2><p>Enhance your game experience with additional digital content, including legendary gear and weapon sets for both Naoe and Yasuke.</p><div class="is-active"><img src="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/20UYtJu5B4St2jVasWOwSz/9492318e3ab35aa92ebde7afc0b645d5/ACSH_Crest_logo_300x378.png" alt="" loading="lazy"><span class="carousel__underline" style="background-color: rgb(158, 12, 18);"></span></div></button></li></ul></div></div><div class="carousel__backward"><button class="carousel__backward__btn" data-ccm-m="buttonMaster" type="button"><svg class="icon icon-arrow-left" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 19 30" role="img" aria-label="Arrow Left"><title>Arrow Left</title><desc>Black arrow pointing left</desc><path d="M15,30L0,15L15,0l4,4L8,15l11,11L15,30z"></path></svg></button></div><div class="carousel__forward"><button class="carousel__forward__btn" data-ccm-m="buttonMaster" type="button"><svg class="icon icon-arrow-right" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 19 30" role="img" aria-label="Arrow Right"><title>Arrow Right</title><desc>Black arrow pointing right</desc><path d="M4,0l15,15L4,30l-4-4l11-11L0,4L4,0z"></path></svg></button></div><div class="carousel__pageCount">3<span>/</span>3</div></div></div>
+	-->
+	
+
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/1XnSPW7uYDTzgOi2aKSvPJ/74782ebda69b5adb9cdcbfe0b3e214a6/Desktop_ACF_Slide1.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-slide-bg"></div>
+				
+				<div class="carousel-slide-content">
+                    <h2>Discover the</br>Digital Deluxe</br>Edition</h2>
+                    <p>Enhance your game experience with additional digital content,</br> including legendary gear and weapon sets for both Naoe and Yasuke.</p>
+                    <a class="custom-btn btn btn1" href="https://store.ubisoft.com/673b49019baa3f5f7f025a29.html" target="_blank">Buy Now</a>
+                    <button class="custom-btn btn" type="button" aria-label="Watch trailer">Watch trailer</button>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/1aecXJZGAIIdettbCPkACO/4636e21dca7a01e41181ca22126989c1/Desktop_ACSH_Takeover_Slider3.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-slide-bg"></div>
+				<div class="carousel-slide-content">
+                    <h2>Next Adventure Awaits</h2>
+                    <p>Get ready for your next big challenge with new missions</br>and rewards. The journey is just beginning.</p>
+                    <a class="custom-btn btn btn1"  href="https://store.ubisoft.com/673b49019baa3f5f7f025a29.html" target="_blank">Buy Now</a>
+                    <button class="custom-btn btn" type="button" aria-label="Watch trailer">Watch trailer</button>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/44rMk6XXwubA02ItkfarO/7a7babadedbd39fa6e89db974f7ca6a2/ACSH_ClawsofAwaji_ACFcarouel.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-slide-bg"></div>
+				<div class="carousel-slide-content">
+                    <h2>Unleash Your Power</h2>
+                    <p>Unlock powerful new gear and embark on an epic journey</br>to change the course of history.</p>
+                    <a class="custom-btn btn btn1" href="https://store.ubisoft.com/673b49019baa3f5f7f025a29.html" target="_blank">Buy Now</a>
+                    <button class="custom-btn btn" type="button" aria-label="Watch trailer">Watch trailer</button>
+                </div>
+            </div>
+        </div>
+
+    
+	    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <svg class="icon icon-arrow-left" x="0px" y="0px" viewBox="0 0 19 30" role="img" aria-label="Arrow Left">
+                <title>Arrow Left</title>
+                <desc>Black arrow pointing left</desc>
+                <path d="M15,30L0,15L15,0l4,4L8,15l11,11L15,30z"></path>
+            </svg>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <svg class="icon icon-arrow-right" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 19 30" role="img" aria-label="Arrow Right">
+                <title>Arrow Right</title>
+                <desc>Black arrow pointing right</desc>
+                <path d="M4,0l15,15L4,30l-4-4l11-11L0,4L4,0z"></path>
+            </svg>
+        </button>
+    </div>
 
 
 	<!-- Hero section -->
-	<section class="hero-section">
+	<!-- <section class="hero-section">
 		<div class="hero-slider owl-carousel">
 			<div class="hs-item set-bg" data-setbg="img/slider-1.jpg">
 				<div class="hs-text">
@@ -59,13 +119,13 @@
 				</div>
 			</div>
 		</div>
-	</section>
+	</section> -->
 	<!-- Hero section end -->
 
 
 	<!-- Latest news section -->
 	<div class="latest-news-section">
-		<div class="ln-title">Latest News</div>
+		<div class="ln-title" style="font-family:PT Serif, serif; text-transform: uppercase">Latest News</div>
 		<div class="news-ticker">
 			<div class="news-ticker-contant">
 				<div class="nt-item"><span class="new">new</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
@@ -78,46 +138,43 @@
 
 
 	<!-- Feature section -->
-	<section class="feature-section spad">
+	<section class="feature-section spad mb-5">
+	    <h2 class="text-center mb-5 h1" style="color:white; font-family:PT Serif, serif;text-transform:uppercase">Trending: Assassin's Creed</br>Shadows announcements</h2>
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-3 col-md-6 p-0">
-					<div class="feature-item set-bg" data-setbg="img/features/1.jpg">
+					<div class="feature-item set-bg" data-setbg="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/2iB4IiCqv7wxcf8SDA1rGq/cd245fc1206d07699c876e64bc4a2ee1/ACFranchise-MoreThanGames-UbisoftGear.jpg">
 						<span class="cata new">new</span>
 						<div class="fi-content text-white">
-							<h5><a href="#">Suspendisse ut justo tem por, rutrum</a></h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-							<a href="#" class="fi-comment">3 Comments</a>
+							<h5><a href="#">Shop The Official Assassin's Creed Merch Collection!</a></h5>
+		
+							<a href="#" class="fi-comment">Buy now</a>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-3 col-md-6 p-0">
-					<div class="feature-item set-bg" data-setbg="img/features/2.jpg">
-						<span class="cata strategy">strategy</span>
+					<div class="feature-item set-bg" data-setbg="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/50kCZyPBfq4irQxZKhuA8N/f5a3fe0289f27b2bb32713dbb7d0199b/ACSH-Rhinoshield-collab.jpg">
 						<div class="fi-content text-white">
-							<h5><a href="#">Justo tempor, rutrum erat id, molestie</a></h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-							<a href="#" class="fi-comment">3 Comments</a>
+							<h5><a href="#">Assassin’s Creed Shadows x RHINOSHIELD – Phone Case Collection</a></h5>
+							<a href="#" class="fi-comment">Buy now</a>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-3 col-md-6 p-0">
-					<div class="feature-item set-bg" data-setbg="img/features/3.jpg">
+					<div class="feature-item set-bg" data-setbg="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/22Ek3V0NdDsy5EevJwdRWd/7f2354c66aa259fd0d99655892c18c2c/acf-merch-bokksu-collab.jpg">
 						<span class="cata new">new</span>
 						<div class="fi-content text-white">
-							<h5><a href="#">Nullam lacinia ex eleifend orci porttitor</a></h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-							<a href="#" class="fi-comment">3 Comments</a>
+							<h5><a href="#">Japan Crate x Assassin’s Creed Shadows — Strike with Flavor Snack Crate</a></h5>
+							<a href="#" class="fi-comment">Buy now</a>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-3 col-md-6 p-0">
-					<div class="feature-item set-bg" data-setbg="img/features/4.jpg">
-						<span class="cata racing">racing</span>
+					<div class="feature-item set-bg" data-setbg="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/6YYI7xdKYBlhJGxAEqGlom/f93d75de9a6bad3fc993b91f04819af8/FF-x-AC-More-Than-Games_Final_web.jpg">
+						<span class="cata racing">limited!</span>
 						<div class="fi-content text-white">
-							<h5><a href="#">Lacinia ex eleifend orci suscipit</a></h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-							<a href="#" class="fi-comment">3 Comments</a>
+							<h5><a href="#">FocusFuel: Limited Edition Citrus Yuzu - with in-game benefits!</a></h5>
+							<a href="#" class="fi-comment">Buy now</a>
 						</div>
 					</div>
 				</div>
@@ -128,22 +185,21 @@
 
 
 	<!-- Recent game section  -->
-	<section class="recent-game-section spad set-bg" data-setbg="img/recent-game-bg.png">
+	<section class="recent-game-section spad set-bg" data-setbg="https://images5.alphacoders.com/136/1362766.jpeg">
 		<div class="container">
 			<div class="section-title">
 				<div class="cata new">new</div>
-				<h2>Recent Games</h2>
+				<h2 class="text-white" style="font-family: PT Serif, serif; text-transform: uppercase">Recent Games</h2>
 			</div>
 			<div class="row">
 				<div class="col-lg-4 col-md-6">
 					<div class="recent-game-item">
-						<div class="rgi-thumb set-bg" data-setbg="img/recent-game/1.jpg">
-							<div class="cata new">new</div>
+						<div class="rgi-thumb set-bg" data-setbg="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/30dd8syYR9JAPhV759qTQe/16542fd1eb739190a9eec72f3d3b08fb/acm-boxshot.jpg?imwidth=360">
+							
 						</div>
-						<div class="rgi-content">
-							<h5>Suspendisse ut justo tem por, rutrum</h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor sit amet, consectetur elit. </p>
-							<a href="#" class="comment">3 Comments</a>
+						<div class="rgi-content text-white text-center">
+							<h5>Assasin’s Creed</br>Mirage</h5>
+							
 							<div class="rgi-extra">
 								<div class="rgi-star"><img src="img/icons/star.png" alt=""></div>
 								<div class="rgi-heart"><img src="img/icons/heart.png" alt=""></div>
@@ -153,13 +209,12 @@
 				</div>
 				<div class="col-lg-4 col-md-6">
 					<div class="recent-game-item">
-						<div class="rgi-thumb set-bg" data-setbg="img/recent-game/2.jpg">
-							<div class="cata racing">racing</div>
+						<div class="rgi-thumb set-bg" data-setbg="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/25jYqtlv4yZXspIIIrPYcy/f90d2f35fcb4f0080a6abbddb5e927ad/AC-Shadows-boxshot.jpg?imwidth=360">
+						    <div class="cata new">new</div>
 						</div>
-						<div class="rgi-content">
-							<h5>Susce pulvinar metus nulla, vel  facilisis sem </h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor sit amet, consectetur elit. </p>
-							<a href="#" class="comment">3 Comments</a>
+						<div class="rgi-content text-white text-center">
+							<h5 >Assassin’s Creed</br>Shadows</h5>
+							
 							<div class="rgi-extra">
 								<div class="rgi-star"><img src="img/icons/star.png" alt=""></div>
 								<div class="rgi-heart"><img src="img/icons/heart.png" alt=""></div>
@@ -169,13 +224,12 @@
 				</div>
 				<div class="col-lg-4 col-md-6">
 					<div class="recent-game-item">
-						<div class="rgi-thumb set-bg" data-setbg="img/recent-game/3.jpg">
-							<div class="cata adventure">Adventure</div>
+						<div class="rgi-thumb set-bg" data-setbg="https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/5EBrXfSnjwvZRpDRtnqmpG/374f103250d4ed61cbb7f2323e063868/ac-valhalla.jpg?imwidth=360">
+							
 						</div>
-						<div class="rgi-content">
-							<h5>Suspendisse ut justo tem por, rutrum</h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisc ing ipsum dolor sit amet, consectetur elit. </p>
-							<a href="#" class="comment">3 Comments</a>
+						<div class="rgi-content text-white text-center">
+							<h5>Assasin’s Creed</br>Valhalla</h5>
+
 							<div class="rgi-extra">
 								<div class="rgi-star"><img src="img/icons/star.png" alt=""></div>
 								<div class="rgi-heart"><img src="img/icons/heart.png" alt=""></div>
@@ -390,5 +444,7 @@
 	<script src="js/owl.carousel.min.js"></script>
 	<script src="js/jquery.marquee.min.js"></script>
 	<script src="js/main.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     </body>
 </html>
