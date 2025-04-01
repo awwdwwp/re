10767fbc073ac54e899a760d963395a64edae56d
awwdwwp <Bobonichandrin@gmail.com>
2025-04-01 20:37:58 +0200

    trying to fix carousel 2x

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 
 	<!-- Stylesheets -->
 	<!-- <link rel="stylesheet" href="css/bootstrap.min.css"/> -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"> -->
 	<!-- <link rel="stylesheet" href="css/font-awesome.min.css"/> -->
 	<!-- <link rel="stylesheet" href="css/owl.carousel.css"/> -->
 	<link rel="stylesheet" href="css/style.css"/>
@@ -28,7 +28,7 @@
 	<![endif]-->
 	<link rel="stylesheet" href="css/carousel.css"/>
 	<link rel="stylesheet" href="css/body.css"/>
-
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
 	<!-- Page Preloder -->
@@ -436,15 +436,15 @@
 	<?php include 'parts/footer.html'; ?>
 	<!-- Footer section end -->
 
-
 	<!--====== Javascripts & Jquery ======-->
 	<script src="js/jquery-3.2.1.min.js"></script>
 	<!-- <script src="js/bootstrap.min.js"></script> -->
 	<!-- <script src="js/owl.carousel.min.js"></script>
 	<script src="js/jquery.marquee.min.js"></script> -->
-	<script src="js/main.js"></script>
+	<!-- <script src="js/main.js"></script> -->
+	<script src="js/new.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 	
     </body>
 </html>
