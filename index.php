46149e9ed7c707e6cdbd01fc688a3e2567bf79dd
awwdwwp <Bobonichandrin@gmail.com>
2025-04-01 20:21:20 +0200

    trying to fix the carousel

--- index.php
+++ index.php
@@ -22,13 +22,12 @@
 	<!-- <link rel="stylesheet" href="css/owl.carousel.css"/> -->
 	<link rel="stylesheet" href="css/style.css"/>
 	<!-- <link rel="stylesheet" href="css/animate.css"/> -->
-	<link rel="stylesheet" href="css/carousel.css"/>
-
-
 	<!--[if lt IE 9]>
 	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+	<link rel="stylesheet" href="css/carousel.css"/>
+	<link rel="stylesheet" href="css/body.css"/>
 
 </head>
 <body>
@@ -440,9 +439,9 @@
 
 	<!--====== Javascripts & Jquery ======-->
 	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/jquery.marquee.min.js"></script>
+	<!-- <script src="js/bootstrap.min.js"></script> -->
+	<!-- <script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.marquee.min.js"></script> -->
 	<script src="js/main.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
--- js/main.js
+++ js/main.js
@@ -16,38 +16,33 @@ $(window).on('load', function() {
 	$(".loader").fadeOut(); 
 	$("#preloder").delay(400).fadeOut("slow");
 
-});
-
+}); 
+/*
 (function($) {
 
-	/*------------------
-		Navigation
-	--------------------*/
+	
 	$('.nav-switch').on('click', function(event) {
 		$('.main-menu').slideToggle(400);
 		event.preventDefault();
 	});
 
 
-	/*------------------
-		Background Set
-	--------------------*/
+	
 	$('.set-bg').each(function() {
 		var bg = $(this).data('setbg');
 		$(this).css('background-image', 'url(' + bg + ')');
 	});
+}) */
 
 
-	/*------------------
-		Hero Slider
-	--------------------*/
-	$('.hero-slider').owlCarousel({
+	
+/*	$('.hero-slider').owlCarousel({
 		loop: true,
 		nav: false,
 		dots: true,
 		mouseDrag: false,
 		animateOut: 'fadeOut',
-    	animateIn: 'fadeIn',
+    animateIn: 'fadeIn',
 		items: 1,
 		autoplay: true
 	});
@@ -62,19 +57,18 @@ $(window).on('load', function() {
 			$(this).append('<span>.</span>');
 		}
 	});
-
+*/
 
 	/*------------------
 		News Ticker
-	--------------------*/
-	$('.news-ticker').marquee({
+	--------------------*/ 
+/*	$('.news-ticker').marquee({
 	    duration: 10000,
-	    //gap in pixels between the tickers
-	    //gap: 200,
+	    
+	    
 	    delayBeforeStart: 0,
 	    direction: 'left',
 	    duplicated: true
 	});
 
-})(jQuery);
-
+})(jQuery); */
